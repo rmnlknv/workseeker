4d5aafef2eb3398560ca806298c9e530c22b918b
rmnlknv <snovix@gmail.com>
2017-06-05 16:41:13 +0200

    Repaired validations on models when user creates, added highlight for error fields

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,9 +1,9 @@
 class Employer < User
 	has_many :vacancies
 
-	validates :name, presence: true, length: { in: 2..26}
-	validates :city, presence: true, length: { in: 2..16 }
-	validates :position, presence: true, length: { in: 2..16 }
-	validates :company, presence: true, length: { in: 2..16 }
-	validates :contact_email, presence: true, email: true
+	validates :name, presence: true, length: { in: 2..26}, on: :update
+	validates :city, presence: true, length: { in: 2..16 }, on: :update
+	validates :position, presence: true, length: { in: 2..16 }, on: :update
+	validates :company, presence: true, length: { in: 2..16 }, on: :update
+	validates :contact_email, presence: true, email: true, on: :update
 end
--- app/models/jobseeker.rb
+++ app/models/jobseeker.rb
@@ -1,12 +1,12 @@
 class Jobseeker < User
 
-	validates :name, presence: true, length: { in: 2..26}
-	validates :city, presence: true, length: { in: 2..16 }
-	validates :position, presence: true, length: { in: 4..16 }
-	validates :category, presence: true
-	validates :salary, presence: true, numericality: true
-	validates :english, presence: true
-	validates :seniority, presence: true, numericality: true
+	validates :name, presence: true, length: { in: 2..26}, on: :update
+	validates :city, presence: true, length: { in: 2..16 }, on: :update
+	validates :position, presence: true, length: { in: 4..16 }, on: :update
+	validates :category, presence: true, on: :update
+	validates :salary, presence: true, numericality: true, on: :update
+	validates :english, presence: true, on: :update
+	validates :seniority, presence: true, numericality: true, on: :update
 
 	scope :salary, -> (min_salary, max_salary) { where("salary BETWEEN ? AND ?", min_salary, max_salary) }
   	scope :city, -> (city) { where city: city }
